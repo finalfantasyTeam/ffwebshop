1144922a77e0ab06876b87575061c5f8bd314009
Squall Taki <taki.lnt@gmail.com>
2016-07-27 00:36:33 +0700

    - Add prototype files.

---
+++ WebShop.EntityFramework/EntityFramework/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using Abp.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Core;
+
+namespace WebShop.EntityFramework.Repositories
+{
+    public class OrderDetailRepository
+    {
+    }
+}
