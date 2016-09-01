cf7bcf222874b99db88c07a387870005c6376317
Tin Ngoc le <tin.ngoc.le@steadfasttech.com.au>
2016-09-01 18:16:03 +0700

    - Add account layout for admin page.

--- WebShop.Web/Controllers/AccountController.cs
+++ WebShop.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using WebShop.Web.Models;
 
 namespace WebShop.Web.Controllers
 {
@@ -6,7 +7,51 @@ namespace WebShop.Web.Controllers
     {
         public ActionResult Index()
         {
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountViewModel viewModel)
+        {
+            return View();
+        }
+
+        public ActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ForgetPassword(AccountViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(AccountViewModel viewModel)
+        {
+            return View(viewModel);
         }
     }
 }
---
+++ WebShop.Web/Models/AccountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Dependency;
+using Castle.MicroKernel.Registration;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using WebShop.Application;
+using WebShop.Core;
+using WebShop.EntityFramework.Repositories;
+
+namespace WebShop.Web.Models
+{
+    public class AccountViewModel
+    {
+        private readonly IUsersAppService _usersAppService;
+    }
+}
